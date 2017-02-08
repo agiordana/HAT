f993cd9cfef86794e837bd72d985844036307708
Attilio Giordana <att.giordana@gmail.com>
2017-02-08 11:39:06 +0100

    Add files via upload

--- agents/websocket/websocket.cpp
+++ agents/websocket/websocket.cpp
@@ -3,13 +3,27 @@
 #define BIN "/home/horus/bin/websocketd"
 using namespace std;
 
+string mkPort(string p) {
+  unsigned wsport = hsrv::a2unsigned(p);
+  wsport++;
+  return hsrv::unsigned2a(wsport);
+}
+
 int main() {
    string conf = CONFIG;
    string cmd;
+   string port;
+   string wsport;
    MParams global;
+   MParams wsglobal;
    string path = conf + "/websocket/global.xml";
+   string wsserverpath = conf + "/wsserver/agentnet.xml";
    global.xmlLoad(path);
+   wsglobal.xmlLoad(wsserverpath);
    cmd = global.get("cmd") + "&";
+   wsport = mkPort(wsglobal.get("port"));
+   hsrv::strReplace(cmd, "$PORT", wsport);
+cout<<cmd<<endl;
    system(cmd.c_str());
    return 0;
  }
